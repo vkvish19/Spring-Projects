d6be85f259f018d2eb0d437024ba8691fb318f79
vkvish19 <vkvish19@gmail.com>
2018-02-12 22:16:48 +0530

    Course API - Initial Commit

---
+++ course-api/src/main/java/vishu/personal/springbootstarter/CourseApiApp.java
@@ -0,0 +1,14 @@
+package vishu.personal.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+	//start main
+	public static void main(String[] args) {
+
+		SpringApplication.run(CourseApiApp.class, args);
+	}
+
+}
